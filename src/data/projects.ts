70a4b7884403be732f9e5f0ed140c79d1c019dde
fracarbo <carbonettof@gmail.com>
2026-03-29 00:41:20 +0100

    feat: add shared mock project data

---
+++ src/data/projects.ts
@@ -0,0 +1,45 @@
+export interface Project {
+  title: string;
+  category: string;
+  year: string;
+  image: string;
+}
+
+export const projects: Project[] = [
+  {
+    title: 'The Shadows Within',
+    category: 'Horror Feature',
+    year: '2024',
+    image: 'https://images.pexels.com/photos/1666471/pexels-photo-1666471.jpeg?auto=compress&cs=tinysrgb&w=800',
+  },
+  {
+    title: 'Echoes of Silence',
+    category: 'Thriller Series',
+    year: '2024',
+    image: 'https://images.pexels.com/photos/2249528/pexels-photo-2249528.jpeg?auto=compress&cs=tinysrgb&w=800',
+  },
+  {
+    title: 'Nightmare Protocol',
+    category: 'Sci-Fi Horror',
+    year: '2023',
+    image: 'https://images.pexels.com/photos/1117132/pexels-photo-1117132.jpeg?auto=compress&cs=tinysrgb&w=800',
+  },
+  {
+    title: 'The Last Séance',
+    category: 'Supernatural',
+    year: '2023',
+    image: 'https://images.pexels.com/photos/1310532/pexels-photo-1310532.jpeg?auto=compress&cs=tinysrgb&w=800',
+  },
+  {
+    title: 'Dark Waters Rise',
+    category: 'Horror Feature',
+    year: '2023',
+    image: 'https://images.pexels.com/photos/1619569/pexels-photo-1619569.jpeg?auto=compress&cs=tinysrgb&w=800',
+  },
+  {
+    title: 'The Forgotten Room',
+    category: 'Mystery Thriller',
+    year: '2022',
+    image: 'https://images.pexels.com/photos/3593922/pexels-photo-3593922.jpeg?auto=compress&cs=tinysrgb&w=800',
+  },
+];
