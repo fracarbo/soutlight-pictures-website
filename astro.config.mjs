fd6689190a07fbc6c4f4ba4f22d0ab01440d77bc
fracarbo <carbonettof@gmail.com>
2026-03-31 19:34:09 +0200

    fix basepath

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fracarbo.github.io',
-  base: '/southlight-pictures-website',
+  base: '/soutlight-pictures-website',
   integrations: [tailwind()],
   vite: {
     preview: {
